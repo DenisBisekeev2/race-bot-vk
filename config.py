8971bacd60722c309d19c371be7110ed025477b4
DenisBisekeev2 <bisekeevdenis6@gmail.com>
2026-01-09 22:09:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ admins_ids = [819016396, 761815201]
 CONFIRMATION_TOKEN = "f9691f4b"  # Токен из настроек Callback API
 SECRET_KEY = "gonkaWow"  # Секретный ключ из настроек Callback API
 GROUP_ID = 233724428
-GITHUB_API_KEY = "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"
+GITHUB_API_KEY = "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"
 
 RACE_DISTANCE = 1000
 GLOBAL_RACE_DISTANCE = 1500
